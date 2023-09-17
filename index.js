89556cf8691cb384975ff07d116b60e0fb02c91f
DanieleLG90 <giacomazzi06@gmail.com>
2023-09-17 11:29:57 +0200

    update
    finito js!

--- index.js
+++ index.js
@@ -3,14 +3,6 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-/*
-let imput = "20"
-let imputConvert = Number(imput)
-const meterFeet = 3.281
-
-let meterFeetConversion = imputConvert * meterFeet
-console.log (meterFeetConversion.toFixed(3))
-*/
 const meterFeet = 3.281
 const literGallons = 0.264
 const kgPound = 2.204
@@ -18,12 +10,16 @@ const kgPound = 2.204
 let inputEl = document.getElementById ("input-el")
 const convertBtn = document.getElementById ("convert-btn")
 let lengthText = document.getElementById ("length-text")
+let volumeText = document.getElementById ("volume-text")
+let massText = document.getElementById ("mass-text")
 
 
 
 
 convertBtn.addEventListener ("click", function (){
     conversion (meterFeet, lengthText, "meters", "ft")
+    conversion (literGallons, volumeText, "liters", "gallons")
+    conversion (kgPound, massText, "kilos", "pounds")
     
 })
 
@@ -33,9 +29,5 @@ function conversion (number, text, par1, par2) {
     
     text.innerHTML = 
         `${inputEl.value} ${par1} = ${firstConv.toFixed(3)} ${par2} | 
-        ${inputEl.value} ${par2} = ${secondConv.toFixed(3)} ${par1} `
-    
-    
-    
-    
+        ${inputEl.value} ${par2} = ${secondConv.toFixed(3)} ${par1} ` 
 }
